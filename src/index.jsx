424b4463a742962715ef9486341002bb349b495f
kloeffler95 <kevin.loeffler@colorado.edu>
2023-09-17 17:47:37 -0600

    Improving formatting.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,20 @@
-import ForgeUI, { render, ContextMenu, InlineDialog, Table, Head, Row, Cell, Text, Strong, Heading, Link, Image, useProductContext, useState } from '@forge/ui';
-import { fetch } from '@forge/api';
-
+import ForgeUI, {
+  render,
+  ContextMenu,
+  InlineDialog,
+  Table,
+  Head,
+  Row,
+  Cell,
+  Text,
+  Strong,
+  Heading,
+  Link,
+  Image,
+  useProductContext,
+  useState,
+} from "@forge/ui";
+import { fetch } from "@forge/api";
 
 /**
  * Set the PQAI API Key as follows:
@@ -9,52 +23,71 @@ import { fetch } from '@forge/api';
  */
 const getPQAIAPIKey = () => {
   return process.env.PQAI_API_KEY;
-}
+};
 
 const App = () => {
-  const { extensionContext: { selectedText } } = useProductContext();
+  const {
+    extensionContext: { selectedText },
+  } = useProductContext();
   const [data] = useState(async () => {
-    const endpoint = "https://api.projectpq.ai"; 
-    const route = "/search/102?"; 
+    const endpoint = "https://api.projectpq.ai";
+    const route = "/search/102?";
     var url = endpoint + route;
-    const q = { q: selectedText, // search query with the user's selected text
-                n: 10, // return 10 results 
-                type: "patent", // exclude research papers
-                token: getPQAIAPIKey() 
-                } 
+    const q = {
+      q: selectedText, // search query with the user's selected text
+      n: 10, // return 10 results
+      type: "patent", // exclude research papers
+      token: getPQAIAPIKey(),
+    };
     url = url + new URLSearchParams(q).toString();
     const response = await fetch(url, {
-      method: 'get',
-      headers: {'Content-Type': 'application/json'}
+      method: "get",
+      headers: { "Content-Type": "application/json" },
     });
     const data = await response.text();
     const res = JSON.parse(data);
     return res;
-  })
+  });
 
   return (
     <InlineDialog>
       <Heading size="large">PriorWise Prior Art Check</Heading>
       <Text>Top 10 patents most similar to your selection:</Text>
       <Table>
-      <Head></Head>
-      {Object.keys(data.results).map(pat => (
-        <Row>
-        <Cell>
-        <Heading size="medium">{data.results[pat].title}</Heading>
-        <Image src={data.results[pat].image}/>
-        <Text><Strong>Author: </Strong>{data.results[pat].alias}</Text>
-        <Text><Strong>Published: </Strong>{data.results[pat].publication_date}</Text>
-        <Text><Strong>ID: </Strong>{data.results[pat].id}</Text>
-        <Text><Strong>Link: </Strong><Link href={data.results[pat].www_link}>{data.results[pat].www_link}</Link></Text>
-        </Cell>
-        </Row>
-      ))}
-    </Table>
+        <Head></Head>
+        {Object.keys(data.results).map((pat) => (
+          <Row>
+            <Cell>
+              <Heading size="medium">{data.results[pat].title}</Heading>
+              <Image src={data.results[pat].image} />
+              <Text>
+                <Strong>Author: </Strong>
+                {data.results[pat].alias}
+              </Text>
+              <Text>
+                <Strong>Published: </Strong>
+                {data.results[pat].publication_date}
+              </Text>
+              <Text>
+                <Strong>ID: </Strong>
+                {data.results[pat].id}
+              </Text>
+              <Text>
+                <Strong>Link: </Strong>
+                <Link href={data.results[pat].www_link}>
+                  {data.results[pat].www_link}
+                </Link>
+              </Text>
+            </Cell>
+          </Row>
+        ))}
+      </Table>
     </InlineDialog>
   );
 };
 
 export const run = render(
-  <ContextMenu><App/></ContextMenu>
+  <ContextMenu>
+    <App />
+  </ContextMenu>
 );
